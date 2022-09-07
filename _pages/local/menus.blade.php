7ed1448f16b45b6d42119724beedced03a346278
Philip <philipxyc@live.com>
2022-09-07 20:52:31 +0000

    1. Customize 2. fix mobile 'add more' button goes to homepage

--- _pages/local/menus.blade.php
+++ _pages/local/menus.blade.php
@@ -3,18 +3,23 @@ title: 'main::lang.local.menus.title'
 permalink: '/:location?local/menus/:category?'
 description: ''
 layout: local
-
 '[localMenu]':
     isGrouped: 1
-    menusPerPage: 200
-    showMenuImages: 0
-    menuImageWidth: 95
-    menuImageHeight: 80
-
+    collapseCategoriesAfter: 5.0
+    menusPerPage: 200.0
+    showMenuImages: 1
+    menuImageWidth: 95.0
+    menuImageHeight: 80.0
+    menuCategoryWidth: 1240.0
+    menuCategoryHeight: 256.0
+    defaultLocationParam: local
+    localNotFoundPage: home
+    hideMenuSearch: 0
+    forceRedirect: 1
 ---
 @partial('nav/local_tabs', ['activeTab' => 'menus'])
 
-<div class="panel">
+<div class="panel" style="margin-bottom: 3rem;">
     <div class="bg-white border-bottom px-3 d-block d-lg-none">
         @partial('categories::mobile')
     </div>
