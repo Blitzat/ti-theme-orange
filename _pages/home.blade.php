a3ba8c6f8ff3b6b9184c899ff8fade84a135dfa4
Philip <philipxyc@live.com>
2022-09-07 20:52:31 +0000

    Customize

--- _pages/home.blade.php
+++ _pages/home.blade.php
@@ -3,22 +3,32 @@ title: 'main::lang.home.title'
 permalink: /
 description: ''
 layout: default
-
 '[slider]':
     code: home-slider
-
 '[localSearch]':
     hideSearch: 0
-
 '[featuredItems]':
     items: ['6', '7', '8', '9']
     limit: 3
     itemsPerRow: 3
     itemWidth: 400
     itemHeight: 300
+'[localList]':
+    alias: '[localList]'
+    distanceUnit: mi
+    searchParamName: search
+    sortByParamName: sort_by
+    orderTypeParamName: order_type
 ---
 @component('slider')
+<div class="container">
+    <div class="row py-4">
+        <div class="location-list">
+            @partial('localList::search')
 
-@component('localSearch')
+            @partial('localList::sorting')
 
-@component('featuredItems')
+            @component('localList')
+        </div>
+    </div>
+</div>
